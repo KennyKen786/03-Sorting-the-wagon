c42b0f3b00d1abed032fa5d99c09d08eb9894cbb
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-08-20 16:36:21 +0100

    Coach_answer

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,29 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.downcase == "i am going to work right now!"
+    return ""
+  elsif your_message.end_with?("?")
+    return "Silly question, get dressed and go to work!"
+  else
+    return "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  return "" if your_message.strip.casecmp("i am going to work right now!").zero?
+
+  shouting = your_message == your_message.upcase && your_message != your_message.downcase
+
+if shouting
+  if your_message.end_with?("?")
+    "I can feel your motivation! Silly question, get dressed and go to work!"
+  else
+    "I can feel your motivation! I don't care, get dressed and go to work!"
+  end
+elsif your_message.end_with?("?")
+  "Silly question, get dressed and go to work!"
+else
+  "I don't care, get dressed and go to work!"
+end
 end
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,13 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+def interactive_coach
+  puts "Welcome to your personal coach!"
+  loop do
+    print "You: "
+    message = gets.chomp
+    response = coach_answer_enhanced(message)
+    puts "Coach: #{response}"
+    break if message.downcase == "i am going to work right now!"
+  end
+  puts "The session is over. Goodbye!"
+end
