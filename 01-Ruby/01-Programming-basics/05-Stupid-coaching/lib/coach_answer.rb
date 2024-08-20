599c5e59fc709b482b75e5001e52ec311140408c
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-08-20 16:49:40 +0100

    Recommitted with the correct solution

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,29 +1,22 @@
+STOP_MESSAGE = "I am going to work right now!"
+
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
-  if your_message.downcase == "i am going to work right now!"
-    return ""
+  if your_message == STOP_MESSAGE || your_message == STOP_MESSAGE.upcase
+    ""
   elsif your_message.end_with?("?")
-    return "Silly question, get dressed and go to work!"
+    "Silly question, get dressed and go to work!"
   else
-    return "I don't care, get dressed and go to work!"
+    "I don't care, get dressed and go to work!"
   end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
-  return "" if your_message.strip.casecmp("i am going to work right now!").zero?
-
-  shouting = your_message == your_message.upcase && your_message != your_message.downcase
-
-if shouting
-  if your_message.end_with?("?")
-    "I can feel your motivation! Silly question, get dressed and go to work!"
+  answer = coach_answer(your_message)
+  if answer == ""
+    ""
+  elsif your_message.upcase == your_message
+    "I can feel your motivation! #{answer}"
   else
-    "I can feel your motivation! I don't care, get dressed and go to work!"
+    answer
   end
-elsif your_message.end_with?("?")
-  "Silly question, get dressed and go to work!"
-else
-  "I don't care, get dressed and go to work!"
-end
 end
