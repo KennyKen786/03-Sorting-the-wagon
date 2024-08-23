c0218894e6cbbdeebfee297698d680980615188a
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-08-23 16:18:59 +0100

    Hash part 2

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
@@ -9,10 +9,22 @@ DISHES_CALORIES = {
   "Lemonade" => 90
 }
 
-def poor_calories_counter(burger, side, beverage)
-  DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
-end
+MEALS = {
+  "Cheesy Combo" => ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
+  "Veggie Combo" => ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
+  "Vegan Combo" => ["Vegan Burger", "Salad", "Lemonade"]
+}
 
 def calories_counter(orders)
-  # TODO: return number of calories for a less constrained order
+  orders.sum { |order| calculate_calories(order) }
+end
+
+def calculate_calories(item)
+  if DISHES_CALORIES.key?(item)
+    DISHES_CALORIES[item]
+  elsif MEALS.key?(item)
+    MEALS[item].sum { |dish| DISHES_CALORIES[dish] }
+  else
+    0
+  end
 end
