1fb8df956a054244eb2dc21d68da4e082b561209
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-08-28 16:30:42 +0100

    Getters abd Setters

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
@@ -1,5 +1,5 @@
 class VendingMachine
-  # TODO: add relevant getter/setter to this class to make the scenarios work properly.
+  attr_accessor :user_balance_cents, :snack_count, :snack_price_cents
 
   def initialize(snack_price_cents, snack_count)
     @user_balance_cents = 0
@@ -8,12 +8,16 @@ class VendingMachine
   end
 
   def insert_coin(input_cents)
-    # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
-    # when the user inserts a coin?
+    @user_balance_cents += input_cents
   end
 
   def buy_snack
-    # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
-    # when the user pushes a button to buy a Snack?
+    if @user_balance_cents >= @snack_price_cents && @snack_count.positive?
+      @snack_count -= 1
+      @user_balance_cents -= @snack_price_cents
+      puts "Snack bought! Enjoy your snack."
+    else
+      puts "Not enough balance or no snacks left."
+    end
   end
 end
