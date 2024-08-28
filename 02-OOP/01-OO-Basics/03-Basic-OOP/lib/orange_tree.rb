3b5e9a9c178021067654cf3ee2c38145e6607c33
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-08-28 18:06:00 +0100

    Orange Tree

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,36 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+  attr_accessor :age, :height, :fruits
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def dead?
+    return @dead
+  end
+
+  def one_year_passes!
+  if @age == 100
+    @dead = true
+  elsif @age > 50
+    @dead = @age > rand(51..100)
+    @age += 1
+  else
+    @age += 1
+    @height += 1 if @age <= 10
+    if @age > 5 && @age < 10
+      @fruits = 100
+    elsif @age >= 10 && @age < 15
+      @fruits = 200
+    else
+      @fruits = 0
+  end
+end
+end
+
+def pick_a_fruit!
+  @fruits -= 1 if @fruits.positive?
+end
 end
