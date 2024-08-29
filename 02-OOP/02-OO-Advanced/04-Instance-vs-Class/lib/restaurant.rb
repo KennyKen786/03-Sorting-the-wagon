0d2a2ca8aa0b82e8b59a01ed9a0cd48f11bed39a
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-08-29 16:50:31 +0100

    Instance vs Class

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,27 @@
 class Restaurant
-  # TODO: add relevant accessors if necessary
+  attr_accessor :city, :name, :average_rating
 
   def initialize(city, name)
-    # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @ratings = []
+    @average_rating = 0.0
   end
 
-  # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @ratings << new_rate
+    @average_rating = @ratings.sum.to_f / @ratings.size
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select { |restaurant| restaurant.city.downcase == city.downcase }
+  end
 end
+
+jules_verne = Restaurant.new("paris", "Jules Verne")
+bluebird = Restaurant.new("london", "Bluebird")
+daniel = Restaurant.new("new york", "Daniel")
+restaurants = [jules_verne, bluebird, daniel]
+
+filtered_restaurants = Restaurant.filter_by_city(restaurants, "london")
+puts filtered_restaurants.inspect
