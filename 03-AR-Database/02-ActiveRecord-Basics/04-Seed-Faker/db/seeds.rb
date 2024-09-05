272fad59adb01450f2e9f3b88ad071529e7afa30
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-09-05 12:59:47 +0100

    Task 4 Seed Faker

--- 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,9 @@
 require "faker"
 # TODO: Write a seed to insert 100 posts in the database
+100.times do
+  Post.create(
+    title: Faker::Book.title,
+    url: Faker::Internet.url,
+    votes: rand(1..100)
+  )
+end
