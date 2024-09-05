db6aae2a0829b30ead8e27e8fc404ca04d630217
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-09-05 14:21:28 +0100

    Task 5 Seed API

--- 03-AR-Database/02-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -2,3 +2,18 @@ require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+top_stories_url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+response = RestClient.get(top_stories_url)
+top_story_ids = JSON.parse(response.body)
+
+top_story_ids.first(10).each do |id|
+  post_url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  post_response = RestClient.get(post_url)
+  post_details = JSON.parse(post_response.body)
+
+  Post.create(
+    title: post_details['title'],
+    url: post_details['url'],
+    votes: post_details['score'] || 0
+  )
+end
