8562613fd567d8c9e535a2c5894d976f25b55ca6
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-09-13 17:01:36 +0100

    My First Event Listener

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,16 @@
 // TODO: React to a click on the button!
+const button = document.getElementById('clickme');
+
+const handleClick = () => {
+  button.classList.add('disabled');
+  button.setAttribute('disabled', true);
+
+  button.textContent = "Bingo!";
+
+  const audio = new Audio('sound.mp3');
+  audio.play().catch((error) => {
+    console.log('Audio playback failed:', error);
+  });
+};
+
+button.addEventListener('click', handleClick);
