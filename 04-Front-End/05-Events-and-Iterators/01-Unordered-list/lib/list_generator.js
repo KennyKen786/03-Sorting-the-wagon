e30935d2a7b93b693e1c66dcd0920ba5bbfee3bd
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-09-13 15:53:12 +0100

    Unordered List

--- 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
+++ 04-Front-End/05-Events-and-Iterators/01-Unordered-list/lib/list_generator.js
@@ -6,17 +6,26 @@ import runChallenges from "../spec/list_generator_examiner.js";
 
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
-};
+  if (items.length === 0) {
+    return `<ul class="list-group"></ul>`;
+  }
 
+  const listItems = items.map(item => listItem(item)).join('');
+
+  return `<ul class="list-group">${listItems}</ul>`;
+};
 // Do not remove these lines:
 if (typeof window === "object") {
   document.addEventListener("DOMContentLoaded", () => {
     // TODO: call unorderedList() passing the right element
     // TODO: display the list in the browser
+    const groceries = ['milk', 'butter', 'bread'];
+    const listElement = document.getElementById('list');
   });
 }
 
