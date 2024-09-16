9172e022962ffbb8764401ee21ab835aead7aa1f
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-09-16 12:46:57 +0100

    Click-me

--- 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
@@ -1,7 +1,11 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
+  const button = document.querySelector('button');
   // TODO: Bind the `click` event to the button
+  button.addEventListener('click', () => {
+  });
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+  alert('Thank you!');
 };
 
 displayAlertOnButtonClick(); // Do not remove!
