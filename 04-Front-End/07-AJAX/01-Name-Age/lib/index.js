9480ba70c9dd9c0f8b7f139fa65f1bd756b277a1
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-09-17 16:19:17 +0100

    Name Age

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,10 +1,30 @@
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-const displayDefinition = (event) => {
+const displayDefinition = async (event) => {
   event.preventDefault();
+
   const word = document.getElementById("word").value;
-  // TODO: Display your word's definition with an AJAX call instead of the console.log()
-  console.log(word);
+
+  const url = `${apiUrl}${word}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Word not found");
+    }
+
+    const data = await response.json();
+
+    const firstDefinition = data[0].meanings[0].definitions[0].definition;
+
+
+    const definitionElement = document.getElementById("definition");
+    definitionElement.innerText = firstDefinition;
+  } catch (error) {
+    const definitionElement = document.getElementById("definition");
+    definitionElement.innerText = `Error: ${error.message}`;
+  }
 };
 
 const form = document.getElementById("fetch-word");
