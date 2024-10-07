ad68ac7663561e935f946c1bc908159ebeeed303
Omar Kayani <77540733+KennyKen786@users.noreply.github.com>
2024-10-08 00:15:01 +0100

    Black-Jack

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -1,4 +1,24 @@
 require_relative "croupier"
+require_relative "black_jack"
 
-# TODO: make the user play from terminal in a while loop that will stop
-#       when the user will not be asking for  a new card
+def play_black_jack
+  bank_score = pick_bank_score
+  player_score = 0
+
+  loop do
+    puts "Card? Type 'Y' or 'yes' to get a new card, or anything else to stop."
+    answer = gets.chomp.downcase
+
+    if ['y', 'yes'].include?(answer)
+      player_score += pick_player_card
+      puts state_of_the_game(player_score, bank_score)
+
+      break if player_score >= 21
+    else
+      break
+    end
+  end
+  puts end_game_message(player_score, bank_score)
+end
+
+play_black_jack
